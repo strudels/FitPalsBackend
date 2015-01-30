145a5f40281744c72ec53be76ec961369c48686e
strudels <ben@uhsome.com>
2015-01-29 19:56:40 -0500

    Add (probably broken) mysql functionality to find messages between users after a certain time

--- RestDatabase/jabber_msg_trigger.sql
+++ RestDatabase/jabber_msg_trigger.sql
@@ -1,3 +1,4 @@
+/*
 delimiter //
 CREATE TRIGGER msg_apn AFTER INSERT ON tig_ma_msgs
 FOR EACH ROW
@@ -6,4 +7,31 @@ BEGIN
     SET result = sys_exec(cmd);
 END;
 //
+delimiter ;
+*/
+
+delimiter //
+CREATE FUNCTION IF NOT EXISTS `jid_to_tig_id` (jabber_id VARCHAR(2049))
+    RETURNS BIGINT(20) UNSIGNED
+BEGIN
+DECLARE tig_id BIGINT(20) UNSIGNED;
+SELECT jid_id INTO tig_id FROM tig_ma_jids WHERE jid=jabber_id;
+RETURN tig_id
+END //
+delimiter ;
+
+delimiter //
+CREATE PROCEDURE IF NOT EXISTS `get_messages`(
+    IN sender VARCHAR(2049), IN receiver VARCHAR(2049), IN time TIMESTAMP)
+BEGIN
+    DECLARE sender_tig; 
+    DECLARE receiver_tig; 
+    SET sender_tig = jid_to_tig_id(sender);
+    SET receiver_tig = jid_to_tig_id(receiver);
+    SELECT msg FROM tig_ma_msgs
+        WHERE ((owner_id=sender_tig AND buddy_id=receiver_tig)
+                OR (owner_id=receiver_tig AND buddy_id=sender_tig))
+            AND ts > time
+        ORDER BY TIMESTAMP;
+END //
 delimiter ;
