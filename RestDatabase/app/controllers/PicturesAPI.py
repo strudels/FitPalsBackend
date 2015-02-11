a946bb4e6328562e7aab594a8d960031187fa2cc
strudels <ben@uhsome.com>
2015-02-11 13:21:53 -0500

    Add doc comments to PicturesAPI.py

--- RestDatabase/app/controllers/PicturesAPI.py
+++ RestDatabase/app/controllers/PicturesAPI.py
@@ -9,14 +9,18 @@ from app import db, api
 from app.models import *
 from app.utils.Response import Response
 
-@api.resource("/users/<user_id>/pictures")
+@api.resource("/users/<int:user_id>/pictures")
 class PicturesAPI(Resource):
     #get all pictures for user_id
     def get(self, user_id):
-        #cast user_id to int type
-        try: user_id = int(user_id)
-        except: 
-            return Response(status=400,message="Invalid user id.").__dict__,400
+        """
+        Get all (secondary)pictures for a user.
+
+        :param int user_id: Id of user.
+
+        :status 400: Could not find user.
+        :status 200: Pictures found.
+        """
 
         #get user from database
         user = User.query.filter(User.id==user_id).first()
@@ -24,23 +28,32 @@ class PicturesAPI(Resource):
             return Response(status=400,
                 message="Could not find user.").__dict__,400
 
-        return Response(status=200, message="Pictures retrieved.",
+        return Response(status=200, message="Pictures found.",
             value=[p.dict_repr() for p in user.secondary_pictures.all()])\
             .__dict__, 200
 
     #add a picture for a user
     def post(self, user_id):
+        """
+        Post new (secondary)picture for a user.
+
+        :reqheader Authorization: fb_id token needed here
+
+        :param int user_id: Id of user.
+
+        :form str picture_id: Facebook Picture Id string.
+
+        :status 401: Not Authorized.
+        :status 400: Could not find user.
+        :status 201: Picture added.
+        """
+
         parser = reqparse.RequestParser()
         parser.add_argument("picture_id",
             type=str, location="form", required=True)
         parser.add_argument("fb_id",
             type=str, location="form", required=True)
         args = parser.parse_args()
-
-        #cast user_id to int type
-        try: user_id = int(user_id)
-        except: 
-            return Response(status=400,message="Invalid user id.").__dict__,400
 
         #get user from database
         user = User.query.filter(User.id==user_id).first()
@@ -50,27 +63,36 @@ class PicturesAPI(Resource):
 
         #ensure user is valid by checking if fb_id is correct
         if user.fb_id != args.fb_id:
-            return Response(status=401,message="Incorrect fb_id.").__dict__,401
+            return Response(status=401,message="Not Authorized.").__dict__,401
 
         picture = Picture(user,args.picture_id)
         user.secondary_pictures.append(picture)
         db.session.commit()
 
-        return Response(status=201, message="Picture stored.").__dict__,201
+        return Response(status=201, message="Picture added.").__dict__,201
 
     #delete either a single, or all pictures for a user
     def delete(self, user_id):
+        """
+        Delete (secondary)picture for a user.
+
+        :reqheader Authorization: fb_id token needed here
+
+        :param int user_id: Id of user.
+
+        :form str picture_id: Facebook Picture Id string.
+
+        :status 401: Not Authorized.
+        :status 400: "Could not find user" or "Picture not found".
+        :status 201: "Picture removed" or "Pictures removed".
+        """
+
         parser = reqparse.RequestParser()
         parser.add_argument("picture_id",
             type=str, location="form", required=False)
         parser.add_argument("fb_id",
             type=str, location="form", required=True)
         args = parser.parse_args()
-
-        #cast user_id to int type
-        try: user_id = int(user_id)
-        except: 
-            return Response(status=400,message="Invalid user id.").__dict__,400
 
         #get user from database
         user = User.query.filter(User.id==user_id).first()
@@ -86,7 +108,7 @@ class PicturesAPI(Resource):
 
         #ensure user is valid by checking if fb_id is correct
         if user.fb_id != args.fb_id:
-            return Response(status=401,message="Incorrect fb_id.").__dict__,401
+            return Response(status=401,message="Not Authorized.").__dict__,401
 
         #get specified picture to delete
         picture = user.secondary_pictures\
