71488b56d8164a70c16c57d7c1ebfe542d58dd0c
strudels <ben@uhsome.com>
2015-02-12 23:13:55 -0500

    Add status code unit tests for PicturesAPI

---
+++ RestDatabase/tests/controllers/PicturesAPI.py
@@ -0,0 +1,51 @@
+import unittest
+import simplejson as json
+from app import app,db
+from app.models import *
+
+class PicturesApiTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+        self.test_user = User.query.filter(User.fb_id=="fbTestUser1").first()
+        if not self.test_user:
+            self.test_user = User("fbTestUser1")
+            db.session.add(self.test_user)
+            db.session.commit()
+
+    def tearDown(self):
+        if hasattr(self, "test_user"):
+            db.session.delete(self.test_user)
+            db.session.commit()
+
+    def test_add_picture(self):
+        fb_id = self.test_user.fb_id
+        db.session.add(self.test_user)
+        resp = self.app.post("/users/" + str(self.test_user.id) + "/pictures",
+            data={"fb_id":fb_id, "picture_id":"some fb picture id string"})
+        db.session.commit()
+        assert resp.status_code==201 #user created
+
+    def test_get_pictures(self):
+        resp = self.app.get("/users/" + str(self.test_user.id) + "/pictures")
+        assert resp.status_code==200
+
+    def test_delete_pictures(self):
+        fb_id = self.test_user.fb_id
+        self.test_user.secondary_pictures.append(Picture(self.test_user,"some fb picture string"))
+        db.session.commit()
+        resp = self.app.delete("/users/" + str(self.test_user.id) + "/pictures",
+            data={"fb_id":fb_id},
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        db.session.commit()
+        assert resp.status_code==200
+
+    def test_delete_one_picture(self):
+        fb_id = self.test_user.fb_id
+        self.test_user.secondary_pictures.append(Picture(self.test_user,"some fb picture string"))
+        db.session.commit()
+        pic = self.test_user.secondary_pictures.first()
+        resp = self.app.delete("/users/" + str(self.test_user.id) + "/pictures",
+            data={"fb_id":fb_id, "picture_id":pic.picture},
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
+        assert resp.status_code==200
