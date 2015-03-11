510770216a6fbd1b8d2a19cbbc1066119a6357ca
strudels <ben@uhsome.com>
2015-03-11 01:52:06 -0400

    Update ActivityAPI.py unit tests to use new FitPalsTestCase superclass

--- RestDatabase/tests/controllers/ActivityAPI.py
+++ RestDatabase/tests/controllers/ActivityAPI.py
@@ -1,40 +1,13 @@
-import unittest
-import simplejson as json
-from app import app,db,socketio,reset_app
-from app.models import *
-from datetime import date
+from tests.utils.FitPalsTestCase import *
 
-class ActivitiesApiTestCase(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
-
-    def tearDown(self):
-        pass
-
+class ActivitySettingsAPITestCase(FitPalsTestCase):
     def test_get_activities(self):
         resp = self.app.get("/activities")
         assert resp.status_code==200
-
-class ActivitySettingsAPITestCase(unittest.TestCase):
-    def setUp(self):
-        reset_app()
-        self.app = app.test_client()
-
-        self.test_user = User.query.filter(User.fb_id=="fbTestUser1").first()
-        if not self.test_user:
-            self.test_user = User("fbTestUser1",dob=date(1990,1,1))
-            db.session.add(self.test_user)
-            db.session.commit()
-        self.test_user_private = self.test_user.dict_repr(public=False)
-        self.test_user = self.test_user.dict_repr()
-
-    def tearDown(self):
-        reset_app()
     
     def test_get_activity_settings(self):
         resp =\
-            self.app.get("/activity_settings?user_id=%d" % self.test_user["id"])
+            self.app.get("/activity_settings?user_id=%d" % self.test_user1["id"])
         assert resp.status_code==200
         assert json.loads(resp.data)["message"]=="Activity settings found."
 
@@ -45,15 +18,11 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"]=="User not found."
         
     def test_create_activity_setting(self):
-        #log in test_user1 to chat web socke
-        client = socketio.test_client(app)
-        client.emit("join", self.test_user_private)
-
         #create activity
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -61,16 +30,16 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert resp.status_code==201
         assert json.loads(resp.data)["message"]=="Activity setting created."
 
-        #ensure that test_user1 websocket client got update
-        received = client.get_received()
+        #ensure that test_user1 websocket self.websocket_client1 got update
+        received = self.websocket_client1.get_received()
         assert len(received) != 0
         assert received[-1]["name"] == "activity_setting_added"
         
     def test_create_activity_setting_question_not_found(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":0,
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -79,7 +48,7 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"]=="Question not found."
 
     def test_create_activity_setting_user_not_found(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
                              data = {"user_id":0,
@@ -91,10 +60,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"]=="User not found."
         
     def test_create_activity_setting_not_authorized(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -103,10 +72,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"]=="Not Authorized."
 
     def test_create_activity_setting_cant_create(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":2.6,},
@@ -116,10 +85,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         
     def test_get_activity_setting(self):
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -139,15 +108,11 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         
 
     def test_update_activity_setting(self):
-        #log in test_user1 to chat web socke
-        client = socketio.test_client(app)
-        client.emit("join", self.test_user_private)
-
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -161,13 +126,13 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert resp.status_code==202
         assert json.loads(resp.data)["message"]=="Activity setting updated."
 
-        #ensure that test_user1 websocket client got update
-        received = client.get_received()
+        #ensure that test_user1 websocket self.websocket_client1 got update
+        received = self.websocket_client1.get_received()
         assert len(received) != 0
         assert received[-1]["name"] == "activity_setting_updated"
 
     def test_update_activity_setting_not_found(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         #update setting
         resp = self.app.put("/activity_settings/%d" % 0,
                             data={"lower_value":4.6, "upper_value":5.7},
@@ -177,10 +142,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
 
     def test_update_activity_setting_not_authorized(self):
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -196,10 +161,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
 
     def test_update_activity_setting_could_not_update(self):
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -214,15 +179,11 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"]=="Could not update activity setting."
         
     def test_delete_activity_setting(self):
-        #log in test_user1 to chat web socke
-        client = socketio.test_client(app)
-        client.emit("join", self.test_user_private)
-
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
@@ -235,13 +196,13 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         assert resp.status_code==200
         assert json.loads(resp.data)["message"]=="Activity setting deleted."
 
-        #ensure that test_user1 websocket client got update
-        received = client.get_received()
+        #ensure that test_user1 websocket self.websocket_client1 got update
+        received = self.websocket_client1.get_received()
         assert len(received) != 0
         assert received[-1]["name"] == "activity_setting_deleted"
 
     def test_delete_activity_setting_not_found(self):
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.delete("/activity_settings/%d" % 0,
                                headers={"Authorization":fb_id})
         assert resp.status_code==404
@@ -249,10 +210,10 @@ class ActivitySettingsAPITestCase(unittest.TestCase):
         
     def test_delete_activity_setting_not_authorized(self):
         #create setting
-        fb_id = self.test_user_private["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         activity = json.loads(self.app.get("/activities").data)["value"][0]
         resp = self.app.post("/activity_settings",
-                             data = {"user_id":self.test_user["id"],
+                             data = {"user_id":self.test_user1["id"],
                                      "question_id":activity["questions"][0]["id"],
                                      "lower_value":8.3,
                                      "upper_value":20.6,},
