8086113e04c7393e3ed412aaecb1da1acf75ea8a
strudels <ben@uhsome.com>
2015-03-11 15:51:28 -0400

    Update UserAPI.py unittests to use new FitPalsTestCase super class

--- RestDatabase/tests/controllers/UserAPI.py
+++ RestDatabase/tests/controllers/UserAPI.py
@@ -1,26 +1,6 @@
-import unittest
-import simplejson as json
-from app import app,db,socketio,reset_app
-from app.models import *
-from datetime import date
+from tests.utils.FitPalsTestCase import *
 
-class UsersApiTestCase(unittest.TestCase):
-    def setUp(self):
-        app.testing = True
-        self.app = app.test_client()
-
-        self.test_user = User.query.filter(User.fb_id=="fbTestUser1").first()
-        if not self.test_user:
-            self.test_user = User("fbTestUser1",dob=date(1990,1,1))
-            self.test_user.name = "test"
-            db.session.add(self.test_user)
-            db.session.commit()
-            self.test_user_public = self.test_user.dict_repr()
-            self.test_user = self.test_user.dict_repr(public=False)
-
-    def tearDown(self):
-        reset_app()
-
+class UsersApiTestCase(FitPalsTestCase):
     def test_create_user(self):
         resp = self.app.post("/users",data={"fb_id":"some fb_id",
                                             "dob_year":1990,
@@ -30,13 +10,13 @@ class UsersApiTestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"] == "User created."
 
     def test_create_user_found(self):
-        resp = self.app.post("/users",data={"fb_id":self.test_user["fb_id"]})
+        resp = self.app.post("/users",data={"fb_id":self.test_user1["fb_id"]})
         assert resp.status_code==200
         assert json.loads(resp.data)["message"] == "User found."
 
     def test_get_users(self):
-        fb_id = self.test_user["fb_id"]
-        setting_id = self.test_user["search_settings_id"]
+        fb_id = self.test_user1["fb_id"]
+        setting_id = self.test_user1["search_settings_id"]
         activity_id = json.loads(self.app.get("/activities").data)["value"][0]["id"]
 
         #update search settings
@@ -50,14 +30,14 @@ class UsersApiTestCase(unittest.TestCase):
         assert resp.status_code==200
         
     def test_get_users_not_authorized(self):
-        fb_id = self.test_user["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.get("/users?longitude=20&latitude=20&radius=17000",
                             headers={"Authorization":fb_id + "junk"})
         assert resp.status_code==401
         
     def test_get_users_invalid_gps_params(self):
-        fb_id = self.test_user["fb_id"]
-        setting_id = self.test_user["search_settings_id"]
+        fb_id = self.test_user1["fb_id"]
+        setting_id = self.test_user1["search_settings_id"]
         activity_id = json.loads(self.app.get("/activities").data)["value"][0]["id"]
 
         #update search settings
@@ -71,15 +51,18 @@ class UsersApiTestCase(unittest.TestCase):
         assert resp.status_code==400
         
     def test_get_user(self):
-        resp = self.app.get("/users/" + str(self.test_user["id"]))
+        test_user1_public = self.test_user1
+        del test_user1_public["fb_id"]
+        del test_user1_public["password"]
+        resp = self.app.get("/users/" + str(self.test_user1["id"]))
         assert resp.status_code==200
-        assert json.loads(resp.data)["value"] == self.test_user_public
+        assert json.loads(resp.data)["value"] == test_user1_public
 
     def test_get_user_name(self):
-        resp = self.app.get("/users/" + str(self.test_user["id"])\
+        resp = self.app.get("/users/" + str(self.test_user1["id"])\
                             + "?attributes=name")
         assert resp.status_code==200
-        assert json.loads(resp.data)["value"] == {"name":self.test_user["name"]}
+        assert json.loads(resp.data)["value"] == {"name":self.test_user1["name"]}
         
     def test_get_user_not_found(self):
         resp = self.app.get("/users/0")
@@ -87,26 +70,22 @@ class UsersApiTestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"] == "User not found."
         
     def test_get_user_authorized(self):
-        resp = self.app.get("/users/" + str(self.test_user["id"]),
-                            headers={"Authorization":self.test_user["fb_id"]})
+        resp = self.app.get("/users/" + str(self.test_user1["id"]),
+                            headers={"Authorization":self.test_user1["fb_id"]})
         assert resp.status_code==200
-        assert json.loads(resp.data)["value"] == self.test_user
+        assert json.loads(resp.data)["value"] == self.test_user1
 
     def test_get_user_not_authorized(self):
-        resp = self.app.get("/users/" + str(self.test_user["id"]),
+        resp = self.app.get("/users/" + str(self.test_user1["id"]),
                             headers={"Authorization":
-                                     self.test_user["fb_id"] + "junk"})
+                                     self.test_user1["fb_id"] + "junk"})
         assert resp.status_code==401
         assert json.loads(resp.data)["message"] == "Not Authorized."
 
     def test_update_user(self):
-        #log in test_user1 to chat web socket
-        client = socketio.test_client(app)
-        client.emit("join", self.test_user)
-
         #update the user
-        user_id = self.test_user["id"]
-        fb_id = self.test_user["fb_id"]
+        user_id = self.test_user1["id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.put("/users/" + str(user_id),
                             data={
                                 "longitude":20,
@@ -115,15 +94,15 @@ class UsersApiTestCase(unittest.TestCase):
                             headers = {"Authorization":fb_id})
         assert resp.status_code==202
         
-        #ensure that test_user websocket client got new user update
-        received = client.get_received()
+        #ensure that test_user websocket self.websocket_client1 got new user update
+        received = self.websocket_client1.get_received()
         assert len(received) != 0
         assert received[-1]["name"] == "user_update"
         assert received[-1]["args"][0] == json.loads(resp.data)["value"]
         
     def test_update_user_not_found(self):
         user_id = 0
-        fb_id = self.test_user["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.put("/users/" + str(user_id),
                             data={
                                 "longitude":20,
@@ -134,8 +113,8 @@ class UsersApiTestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"] == "Could not find user."
         
     def test_update_user_not_authorized(self):
-        user_id = self.test_user["id"]
-        fb_id = self.test_user["fb_id"] + "junk"
+        user_id = self.test_user1["id"]
+        fb_id = self.test_user1["fb_id"] + "junk"
         resp = self.app.put("/users/" + str(user_id),
                             data={
                                 "longitude":20,
@@ -146,8 +125,8 @@ class UsersApiTestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"] == "Not Authorized."
         
     def test_delete_user(self):
-        user_id = self.test_user["id"]
-        fb_id = self.test_user["fb_id"]
+        user_id = self.test_user1["id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.delete("/users/" + str(user_id),
                                headers={"Content-Type": "application/x-www-form-urlencoded",
                                         "Authorization":fb_id})
@@ -156,7 +135,7 @@ class UsersApiTestCase(unittest.TestCase):
         
     def test_delete_user_not_found(self):
         user_id = 0
-        fb_id = self.test_user["fb_id"]
+        fb_id = self.test_user1["fb_id"]
         resp = self.app.delete("/users/" + str(user_id),
                                headers={"Content-Type": "application/x-www-form-urlencoded",
                                         "Authorization":fb_id})
@@ -164,8 +143,8 @@ class UsersApiTestCase(unittest.TestCase):
         assert json.loads(resp.data)["message"] == "Could not find user."
 
     def test_delete_user_not_authorized(self):
-        user_id = self.test_user["id"]
-        fb_id = self.test_user["fb_id"] + "junk"
+        user_id = self.test_user1["id"]
+        fb_id = self.test_user1["fb_id"] + "junk"
         resp = self.app.delete("/users/" + str(user_id),
                                headers={"Content-Type": "application/x-www-form-urlencoded",
                                         "Authorization":fb_id})
