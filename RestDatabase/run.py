aab86c75d5a0fe5a9961f3e025fc7dd5ef9276ac
strudels <ben@uhsome.com>
2015-04-10 03:28:27 -0400

    Put SIGINT handler in run.py so that when Ctrl-C is hit, the AsyncNotifications thread will not hang forever.

--- RestDatabase/run.py
+++ RestDatabase/run.py
@@ -1,7 +1,15 @@
 #from app import app, db, models, controllers
 #from app import app,db
 import sys
+import signal
 from app import socketio, app
+from app.utils import AsyncNotifications
+
+def sigint_handler(signal, frame):
+    #kill thread responsible for cleaning up async call threads
+    AsyncNotifications.manager_thread._Thread__stop()
+    sys.exit()
+signal.signal(signal.SIGINT, sigint_handler)
 
 # > python run.py
 if len(sys.argv) == 1:
@@ -15,3 +23,6 @@ elif sys.argv[1] == "setup":
     from app import reset_app
     reset_app()
     print "Setup complete."
+    AsyncNotifications.manager_thread._Thread__stop()
+    
+    
